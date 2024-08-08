91e976b0ac62816420c01b804b93d1a761374b85
NeoFlutterDev <neojfc1@gmail.com>
2024-08-08 17:04:46 +0100

    updated class getter and setters

--- classes.py
+++ classes.py
@@ -5,6 +5,9 @@ class Race:
         self.exp = exp
         self.lvl = math.trunc(2.5 * (math.sqrt(self.exp) - 5))
         self.armour = None
+        self.armourModifier = 1
+        self.weapon = None
+        self.weaponModifier = 1
         self.atk = math.trunc(1.5 * self.lvl + 100)
         self.maxHp = math.trunc(((self.lvl / 4.624) + 10)**2)
         self.currentHp = self.maxHp
@@ -27,6 +30,24 @@ class Race:
 
     def get_armour(self):
         return self.armour
+    
+    def set_armourModifier(self, armourModifier):
+        self.armourModifier = armourModifier
+
+    def get_armourModifier(self):
+        return self.armourModifier
+    
+    def set_weapon(self, weapon):
+        self.weapon = weapon
+
+    def get_weapon(self):
+        return self.weapon
+    
+    def set_weaponModifier(self, weaponModifier):
+        self.weaponModifier = weaponModifier
+
+    def get_weaponModifier(self):
+        return self.weaponModifier
 
     def update_atk(self):
         self.atk = math.trunc(1.5 * self.lvl + 100)
@@ -54,7 +75,7 @@ class Race:
         return self.maxStm
 
     def update_currentStm(self, stmGain):
-        self.currentStm = self.currentStm = stmGain
+        self.currentStm = self.currentStm + stmGain
         #if stamina has been lost enter a negative value for stmGain
 
     def get_currentStm(self):
