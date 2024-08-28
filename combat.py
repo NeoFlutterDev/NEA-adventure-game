59be74c141e033449e618e622fe834ef1efe201e
NeoFlutterDev <neojfc1@gmail.com>
2024-08-28 19:49:27 +0100

    combat mechanics

--- combat.py
+++ combat.py
@@ -1,30 +1,56 @@
 import random
+from classes import Human
 
 def player_combat(player, monster, button_pressed, monsterDodgeValue = 75):
-    if button_pressed == 'attack' and player.get_currentStm >= 5:
-        player.set_currentStm(-5)
+    playerStm = player.get_currentStm
+    playerMaxStm = player.get_maxStm
+    playerAtk = player.get_atk
+    playerWpnMod = player.get_weaponModifier
+    monsterAmrMod = monster.get_armourModifier
+    if button_pressed == 'attack' and playerStm >= 5:
+        player.update_currentStm(-5)
+        playerStm -= 5
         if random.randint(1, 100) < monsterDodgeValue:
-            monster.set_currentHp(-(player.get_atk * player.get_weaponModifier * monster.get_armourModifier * monsterDodgeValue))
-        playerDodgeValue = 75
-    elif button_pressed == 'heavy attack' and player.get_currentStm >= 15:
-        player.set_currentStm(-15)
+            monster.update_currentHp(-(playerAtk * playerWpnMod * monsterAmrMod))
+        return 75
+    #the player dodge value
+    elif button_pressed == 'heavy attack' and playerStm >= 15:
+        player.update_currentStm(-15)
         if random.randint(1, 100) < monsterDodgeValue + 15:
-            monster.set_currentHp(-(player.get_atk * 1.5 * player.get_weaponModifier * monster.get_armourModifier * monsterDodgeValue))
-        playerDodgeValue = 100
+            monster.update_currentHp(-(playerAtk * 1.5 * playerWpnMod * monsterAmrMod))
+        return 100
     elif button_pressed == 'dodge':
         player.set_currentStm(25)
-        if player.get_currentStm > player.get_maxStm:
-            player.set_currentStm(player.get_maxStm)
-        playerDodgeValue = 25
-    elif button_pressed == 'special':
+        if playerStm > playerMaxStm:
+            player.set_currentStm(playerMaxStm)
+        return 25
+    elif button_pressed == 'special' and playerStm >= 50:
         player.special_atk()
 
 def monster_combat(player, monster, playerDodgeValue = 75):
-    if monster.get_currentStm <= 5:
-        monster.set_currentStm(25)
-        if monster.get_currentStm > monster.get_maxStm:
-            monster.set_currentStm(monster.get_maxStm)
-        monsterDodgeValue = 25
-    
+    bias = monster.get_bias
+    monsterStm = monster.get_currentStm
+    monsterMaxStm = monster.get_maxStm
+    monsterAtk = monster.get_atk
+    monsterWpnMod = monster.get_weaponModifier
+    playerAmrMod = player.get_armourModifier
+    #changes the chance, higher bias = more likely to use specials/heavy attacks
+    #lower bias = more likely to dodge or attack, monsters with no special will have a bias of -25 or lower
+    randomSelector = random.randint(1, 100) + bias
+    monsterStm = monster.get_currentStm
+    if randomSelector > 75 and monsterStm >= 50:
+        monster.special()
+    elif randomSelector > 50 and monsterStm >= 15:
+        monsterStm -= 15
+        if random.randint(1, 100) < playerDodgeValue + 15:
+            player.update_currentHp(-(monsterAtk * 1.5 * monsterWpnMod * playerAmrMod))
+    elif randomSelector < 26 and monsterStm >= 5:
+        monsterStm -= 5
+        if random.randint(1, 100) < playerDodgeValue:
+            player.update_currentHp(-(monsterAtk * monsterWpnMod * playerAmrMod))
+    else:
+        player.set_currentStm(25)
+        if monsterStm > monsterMaxStm:
+            monster.set_currentStm(monsterMaxStm)
 
 player_combat(Human(100), Human(20), 'attack')
