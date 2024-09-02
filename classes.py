23aea5982530584b193022d23466fcc949ca5914
NeoFlutterDev <neojfc1@gmail.com>
2024-09-02 19:23:10 +0100

    added bias

--- classes.py
+++ classes.py
@@ -1,6 +1,7 @@
 import math
+import random
 
-class Race:
+class PlayableCharacter:
     def __init__(self, exp):
         self.exp = exp
         self.lvl = math.trunc(2.5 * (math.sqrt(self.exp) - 5))
@@ -64,7 +65,7 @@ class Race:
     def get_maxHp(self):
         return self.maxHp
 
-    def set_currentHp(self, hpGain):
+    def update_currentHp(self, hpGain):
         self.currentHp = self.currentHp + hpGain
         #if health has been lost enter a negative value for hpGain
 
@@ -85,20 +86,107 @@ class Race:
         return self.currentStm
         
         
-class Human(Race):
+class Human(PlayableCharacter):
     def __init__(self, exp):
         super().__init__(exp)
-        self.maxHp = self.maxHp * 1.5     
+        self.maxHp = self.maxHp * 1.5
 
-class Elf(Race):
-    pass
+    def special():
+        pass     
 
-class Insectoid(Race):
-    pass
+class Elf(PlayableCharacter):
+    def special():
+        pass  
 
-class Warrior(Race):
-    pass
+class Insectoid(PlayableCharacter):
+    def special():
+        pass  
+
+class Warrior(PlayableCharacter):
+    def special():
+        pass  
 
 human = Human(25)
 print(human.get_maxHp())
 print(human.get_lvl())
+
+weapon = ['sword', 'axe', 'halberd', 'hammer', 'spear']
+armour = ['chain', 'plate', 'fur']
+rarity = ['novice', 'standard', 'great', 'legendary']
+typeConverter = {'grunt':[[1, 2], -30], 'miniboss':[[2, 3], -15], 'boss':[[4, 4], 15]}
+rarityConverter = {'n': 1.05, 's':1.15, 'g':1.25, 'l':1.4}
+
+class Monster:
+    def __init__(self, playerLvl, type):
+        self.lvl = playerLvl + random.randint(-5, 5)
+        self.armour = f'{rarity[random.randint((typeConverter[type][0])[0], (typeConverter[type][0])[1])]} {armour[random.randint(0, 2)]}'
+        self.armourModifier = 2 - rarityConverter[self.armour[0]] 
+        self.weapon = f'{rarity[random.randint((typeConverter[type][0])[0], (typeConverter[type][0])[1])]} {weapon[random.randint(0, 4)]}'
+        self.weaponModifier = rarityConverter[self.armour[0]]
+        self.bias = typeConverter[type][1]
+        self.atk = math.trunc(1.5 * self.lvl + 100)
+        self.maxHp = math.trunc(((self.lvl / 4.624) + 10)**2)
+        self.currentHp = self.maxHp
+        self.maxStm = math.trunc(4 * self.lvl + 100)
+        self.currentStm = self.maxStm
+        self.type = type
+        #whether the enemy is a grunt, miniboss or boss
+    
+    def get_lvl(self):
+        return self.lvl
+
+    def set_armour(self, armour):
+        self.armour = armour
+
+    def get_armour(self):
+        return self.armour
+    
+    def set_armourModifier(self, armourModifier):
+        self.armourModifier = armourModifier
+
+    def get_armourModifier(self):
+        return self.armourModifier
+    
+    def set_weapon(self, weapon):
+        self.weapon = weapon
+
+    def get_weapon(self):
+        return self.weapon
+    
+    def set_weaponModifier(self, weaponModifier):
+        self.weaponModifier = weaponModifier
+
+    def get_weaponModifier(self):
+        return self.weaponModifier
+
+    def update_atk(self):
+        self.atk = math.trunc(1.5 * self.lvl + 100)
+
+    def get_atk(self):
+        return self.atk
+    
+    def update_maxHp(self):
+        self.maxHp = math.trunc(((self.lvl / 4.624) + 10)**2)
+
+    def get_maxHp(self):
+        return self.maxHp
+
+    def update_currentHp(self, hpGain):
+        self.currentHp = self.currentHp + hpGain
+        #if health has been lost enter a negative value for hpGain
+
+    def get_currentHp(self):
+        return self.currentHp
+    
+    def update_maxStm(self):
+        self.maxStm = math.trunc(4 * self.lvl + 100)
+
+    def get_maxStm(self):
+        return self.maxStm
+
+    def update_currentStm(self, stmGain):
+        self.currentStm = self.currentStm + stmGain
+        #if stamina has been lost enter a negative value for stmGain
+
+    def get_currentStm(self):
+        return self.currentStm
